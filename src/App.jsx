b4d094de8d6b5ce9b19929abf2d113f8a1019de4
Hardy <hardsyldev@gmail.com>
2024-12-01 21:48:48 +0300

    display component About

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
+import About from "./components/About"
 import Hero from "./components/Hero"
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden" >
       <Hero/>
-
-      <section className="h-dvh w-screen bg-blue-500"></section>
+      <About/>
     </main>
   )
 }
