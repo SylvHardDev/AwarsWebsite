dedef4fca6576af1ad1506f2209c47d0d91e10a5
Hardy <hardsyldev@gmail.com>
2024-12-02 22:19:40 +0300

    vite card ray

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 
-const BentoCard = ({ src, title, description, isComingSoon }) => {
+const BentoCard = ({ src, title, description }) => {
   return (
     <div className='relative size-full'>
       <video
@@ -11,6 +11,14 @@ const BentoCard = ({ src, title, description, isComingSoon }) => {
         autoPlay
         className="absolute left-0 top-0 size-full object-cover object-center"
       />
+      <div className="relative z-10 flex size-full flex-col justify-between p-5 text-blue-50">
+        <div>
+          <h1 className="bento-title special-font">{title}</h1>
+          {description && (
+            <p className="mt-3 max-w-64 text-xs md:text-base">{description}</p>
+          )}
+        </div>
+      </div>
     </div>
   )
 }
@@ -38,7 +46,6 @@ const Features = () => {
               </>
             }
             description="A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure."
-            isComingSoon
           />
         </div>
       </div>
