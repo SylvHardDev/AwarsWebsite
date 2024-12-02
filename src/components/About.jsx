5e6a847130a892cd804dce9e6d0632b6d107b8b1
Hardy <hardsyldev@gmail.com>
2024-12-02 13:04:13 +0300

    start animated title*

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/all";
 
-// import AnimatedTitle from "./AnimatedTitle";
+import AnimatedTitle from "./AnimatedTitle";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -33,9 +33,8 @@ const About = () => {
         <h2 className="font-general text-sm uppercase md:text-[10px]">
           Welcome to Zentry
         </h2>
-        <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
-          Disc<b>o</b>ver the world's <br /> l<b>a</b>rgest shared adventure
-        </div>
+        <AnimatedTitle/>
+        
         <div className="about-subtext">
           <p>
             The game of Games begins-your life, now an epic MMORPG
