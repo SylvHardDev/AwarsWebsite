5f8fd7cd1279be1c61f5f059581c4ad767133786
Hardy <hardsyldev@gmail.com>
2024-12-02 12:32:38 +0300

    continuer about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      
+    <div id="about" className="min-h-screen w-screen" >
+      <p className="font-general text-sm uppercase md:text-[10px]">
+        Welcome to Zentry
+      </p>
+      <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
+        <div>
+          
+        </div>
+      </div>
     </div>
   )
 }
