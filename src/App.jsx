0e2dce25113354227c9f94ab0ae9010a0557a593
Hardy <hardsyldev@gmail.com>
2024-12-03 13:10:06 +0300

    import component story

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import About from "./components/About"
 import Features from "./components/Features"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
+import Story from "./components/Story"
 
 const App = () => {
   return (
@@ -10,7 +11,8 @@ const App = () => {
       <Hero />
       <About />
       <Features />
-    </main>
+      <Story/>
+    </main> 
   )
 }
 
