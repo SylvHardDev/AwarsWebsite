7599caaf1e294ba02bfd9c06352943c4630ebdf8
Hardy <hardsyldev@gmail.com>
2024-12-03 15:10:59 +0300

    finish contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import AnimatedTitle from "./AnimatedTitle.jsx"
+import Button from './Button.jsx';
 
 const Contact = () => {
 
@@ -21,6 +23,28 @@ const Contact = () => {
             src="/img/contact-2.webp"
             clipClass="contact-clip-path-2 lg:translate-y-40 translate-y-60"
           />
+        </div>
+        <div className="absolute -top-40 left-20 w-60 sm:top-1/2 md:left-auto md:right-10 lg:top-20 lg:w-80">
+          <ImageClipBox
+            src="/img/swordman-partial.webp"
+            clipClass="absolute md:scale-125"
+          />
+          <ImageClipBox
+            src="/img/swordman.webp"
+            clipClass="sword-man-clip-path md:scale-125"
+          />
+        </div>
+        <div className="flex flex-col items-center text-center">
+          <p className="mb-10 font-general text-[10px] uppercase">
+            Join Zentry
+          </p>
+
+          <AnimatedTitle
+            title="let&#39;s b<b>u</b>ild the <br /> new era of <br /> g<b>a</b>ming t<b>o</b>gether."
+            className="special-font !md:text-[6.2rem] w-full font-zentry !text-5xl !font-black !leading-[.9]"
+          />
+
+          <Button title="contact us" containerClass="mt-10 cursor-pointer" />
         </div>
       </div>
     </div>
